237ea459baa82ba29b4ab0874a97346ca34709cc
Scott Otterson <pcqzd21yhcv97s2i704qivdofgk6v9@bots.bitbucket.org>
2025-02-13 08:54:59 -0800

    obsidian backup auto: 2025-02-13 08:54:59
    Affected files:
    obsidian/Obsidian Share Vault/.obsidian/workspace.json
    obsidian/Obsidian Share Vault/Obsidian/custom_javascripts/outline_pane_toggle.js

--- obsidian/Obsidian Share Vault/Obsidian/custom_javascripts/outline_pane_toggle.js
+++ obsidian/Obsidian Share Vault/Obsidian/custom_javascripts/outline_pane_toggle.js
@@ -1,8 +1,12 @@
-// Toggle Outline Pane
-const outlinePane = app.workspace.getLeavesOfType("outline")[0];
-
-if (outlinePane) {
-    outlinePane.detach(); // Close the Outline pane if it's open
-} else {
-    app.commands.executeCommandById("outline:open"); // Open the Outline pane if it's not open
+class ToggleOutline {
+    async invoke() {
+        const outlinePane = app.workspace.getLeavesOfType("outline")[0];
+        if (outlinePane) {
+            outlinePane.detach(); // Close the Outline pane if it's open
+        } else {
+            app.commands.executeCommandById("outline:open"); // Open the Outline pane if it's not open
+        }
+    }
 }
+
+module.exports = new ToggleOutline();
